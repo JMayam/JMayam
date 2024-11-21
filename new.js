b93976f53b58a07a132360fbe833c0bd212bbf41
JMayam <jmayam_220000001833@uic.edu.ph>
2024-11-22 03:40:25 +0800

    erwerwe
    rwerw

---
+++ new.js
@@ -0,0 +1,6 @@
+const sound = document.querySelector(".sound");
+const playb = document.querySelector(".play");
+const pauseb = document.querySelector(".pause");
+
+playb.addEventListener("click", () => { sound.play});
+console.log(audio);
